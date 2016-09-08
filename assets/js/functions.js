df8a9cae14ad85c32e631641ec7a2e6c76447904
Richard Warren <rwarren92@hotmail.com>
2016-09-08 22:07:24 +0100

    change images

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -19,7 +19,7 @@ if (contactImage) {  jQuery('#contact').css({ 'background-image':'url(' + contac
 /* Document Ready function
 -------------------------------------------------------------------*/
 jQuery(document).ready(function($) {
-	"use strict";
+	                                                                                                                        "use strict";
 
 
     /* Window Height Resize
@@ -27,72 +27,72 @@ jQuery(document).ready(function($) {
     var windowheight = jQuery(window).height();
     if(windowheight > 650)
     {
-         $('.pattern').removeClass('height-resize');
+        $('.pattern').removeClass('height-resize');
     }
     /* Window Height Resize End
     -------------------------------------------------------------------*/
 
 
-    
-	/* Main Menu   
+
+	/* Main Menu
 	-------------------------------------------------------------------*/
-	$('#main-menu #headernavigation').onePageNav({
-		currentClass: 'active',
-		changeHash: false,
-		scrollSpeed: 750,
-		scrollThreshold: 0.5,
-		scrollOffset: 0,
-		filter: '',
-		easing: 'swing'
-	});  
+    $('#main-menu #headernavigation').onePageNav({
+	                                                                            currentClass: 'active',
+	                                                                            changeHash: false,
+	                                                                            scrollSpeed: 750,
+	                                                                            scrollThreshold: 0.5,
+	                                                                            scrollOffset: 0,
+	                                                                            filter: '',
+	                                                                            easing: 'swing'
+	                                    });
 
-	/* Main Menu End  
+	/* Main Menu End
 	-------------------------------------------------------------------*/
 
 
 
 
-	/* Time Countdown 
+	/* Time Countdown
 	-------------------------------------------------------------------*/
-	$('#time_countdown').countDown({
-        
-        // targetDate: {
-        //     'day': 30,
-        //     'month': 9,
-        //     'year': 2015,
-        //     'hour': 0,
-        //     'min': 0,
-        //     'sec': 0
-        // },
-        // omitWeeks: true
+    $('#time_countdown').countDown({
 
-         targetOffset: {
-            'day':      0,
-            'month':    0,
-            'year':     1,
-            'hour':     0,
-            'min':      0,
-            'sec':      3
-		},
-		omitWeeks: true
+        targetDate: {
+            'day': 30,
+            'month': 10,
+            'year': 2016,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        omitWeeks: true
 
-	    });
+        //  targetOffset: {
+        //     'day':      0,
+        //     'month':    0,
+        //     'year':     1,
+        //     'hour':     0,
+        //     'min':      0,
+        //     'sec':      3
+		// },
+		// omitWeeks: true
+
+	                                        });
 
 
 
-	/* Next Section   
+	/* Next Section
 	-------------------------------------------------------------------*/
-	$('.next-section .go-to-about').click(function() {
+	                                                                                                                        $('.next-section .go-to-about').click(function() {
     	$('html,body').animate({scrollTop:$('#about').offset().top}, 1000);
+});
+  	                                                            $('.next-section .go-to-subscribe').click(function() {
+    	                                                            $('html,body').animate({scrollTop:$('#subscribe').offset().top}, 1000);
   	});
-  	$('.next-section .go-to-subscribe').click(function() {
-    	$('html,body').animate({scrollTop:$('#subscribe').offset().top}, 1000);
+  	                                                            $('.next-section .go-to-contact').click(function() {
+    	                                                            $('html,body').animate({scrollTop:$('#contact').offset().top}, 1000);
   	});
-  	$('.next-section .go-to-contact').click(function() {
-    	$('html,body').animate({scrollTop:$('#contact').offset().top}, 1000);
-  	});
-  	$('.next-section .go-to-page-top').click(function() {
-    	$('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
+  	                                                            $('.next-section .go-to-page-top').click(function() {
+    	                                                            $('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
   	});
 
   	/* Next Section End
@@ -105,17 +105,17 @@ jQuery(document).ready(function($) {
   -------------------------------------------------------------------*/
     $(".news-letter").ajaxChimp({
         callback: mailchimpResponse,
-        url: "http://jeweltheme.us10.list-manage.com/subscribe/post?u=a3e1b6603a9caac983abe3892&amp;id=257cf1a459" // Replace your mailchimp post url inside double quote "".  
+        url: "http://jeweltheme.us10.list-manage.com/subscribe/post?u=a3e1b6603a9caac983abe3892&amp;id=257cf1a459" // Replace your mailchimp post url inside double quote "".
     });
 
     function mailchimpResponse(resp) {
-         if(resp.result === 'success') {
-         
+        if(resp.result === 'success') {
+
             $('.alert-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
-            
+
         } else if(resp.result === 'error') {
             $('.alert-warning').html(resp.msg).fadeIn().delay(3000).fadeOut();
-        }  
+        }
     };
 
 
@@ -130,105 +130,105 @@ jQuery(document).ready(function($) {
 	/* Contact
 	-------------------------------------------------------------------*/
     // Email from Validation
-  $('#contact-submit').click(function(e){ 
+    $('#contact-submit').click(function(e){
 
     //Stop form submission & check the validation
-    e.preventDefault();
+        e.preventDefault();
 
 
-    $('.first-name-error, .last-name-error, .contact-email-error, .contact-subject-error, .contact-message-error').hide();
+        $('.first-name-error, .last-name-error, .contact-email-error, .contact-subject-error, .contact-message-error').hide();
 
     // Variable declaration
-    var error = false;
-    var k_first_name = $('#first_name').val();
-    var k_last_name = $('#last_name').val();
-    var k_email = $('#contact_email').val(); 
-    var k_subject = $('#subject').val(); 
-    var k_message = $('#message').val();
+        var error = false;
+        var k_first_name = $('#first_name').val();
+        var k_last_name = $('#last_name').val();
+        var k_email = $('#contact_email').val();
+        var k_subject = $('#subject').val();
+        var k_message = $('#message').val();
 
     // Form field validation
-    if(k_first_name.length == 0){
-      var error = true; 
-      $('.first-name-error').html('<i class="fa fa-exclamation"></i> First name is required.').fadeIn();
-    }  
+        if(k_first_name.length == 0){
+            var error = true;
+            $('.first-name-error').html('<i class="fa fa-exclamation"></i> First name is required.').fadeIn();
+        }
 
-    if(k_last_name.length == 0){
-      var error = true;
-      $('.last-name-error').html('<i class="fa fa-exclamation"></i> Last name is required.').fadeIn();
-    }  
+        if(k_last_name.length == 0){
+            var error = true;
+            $('.last-name-error').html('<i class="fa fa-exclamation"></i> Last name is required.').fadeIn();
+        }
 
-    if(k_email.length != 0 && validateEmail(k_email)){
-       
-    } else {
-      var error = true; 
-      $('.contact-email-error').html('<i class="fa fa-exclamation"></i> Please enter a valid email address.').fadeIn();
-    }
+        if(k_email.length != 0 && validateEmail(k_email)){
 
-    if(k_subject.length == 0){
-      var error = true;
-     $('.contact-subject-error').html('<i class="fa fa-exclamation"></i> Subject is required.').fadeIn();
-    } 
+        } else {
+            var error = true;
+            $('.contact-email-error').html('<i class="fa fa-exclamation"></i> Please enter a valid email address.').fadeIn();
+        }
 
-    if(k_message.length == 0){
-      var error = true;
-      $('.contact-message-error').html('<i class="fa fa-exclamation"></i> Please provide a message.').fadeIn();
-    }  
+        if(k_subject.length == 0){
+            var error = true;
+            $('.contact-subject-error').html('<i class="fa fa-exclamation"></i> Subject is required.').fadeIn();
+        }
+
+        if(k_message.length == 0){
+            var error = true;
+            $('.contact-message-error').html('<i class="fa fa-exclamation"></i> Please provide a message.').fadeIn();
+        }
 
     // If there is no validation error, next to process the mail function
-    if(error == false){
+        if(error == false){
 
-        $('#contact-submit').hide();
-        $('#contact-loading').fadeIn();
-        $('.contact-error-field').fadeOut();
+            $('#contact-submit').hide();
+            $('#contact-loading').fadeIn();
+            $('.contact-error-field').fadeOut();
 
 
       // Disable submit button just after the form processed 1st time successfully.
-      $('#contact-submit').attr({'disabled' : 'true', 'value' : 'Sending' });
+            $('#contact-submit').attr({'disabled' : 'true', 'value' : 'Sending' });
 
       /* Post Ajax function of jQuery to get all the data from the submission of the form as soon as the form sends the values to email.php*/
-      $.post("php/contact.php", $("#contact-form").serialize(),function(result){
+            $.post("php/contact.php", $("#contact-form").serialize(),function(result){
         //Check the result set from email.php file.
-        if(result == 'sent'){
+                if(result == 'sent'){
 
 
 
           //If the email is sent successfully, remove the submit button
-          $('#first_name').remove();
-          $('#last_name').remove(); 
-          $('#contact_email').remove();
-          $('#subject').remove(); 
-          $('#message').remove();
-          $('#contact-submit').remove(); 
+                    $('#first_name').remove();
+                    $('#last_name').remove();
+                    $('#contact_email').remove();
+                    $('#subject').remove();
+                    $('#message').remove();
+                    $('#contact-submit').remove();
 
-          $('.contact-box-hide').slideUp();
-          $('.contact-message').html('<i class="fa fa-check contact-success"></i><div>Your message has been sent.</div>').fadeIn();
-        } else {
-          $('.btn-contact-container').hide();
-          $('.contact-message').html('<i class="fa fa-exclamation contact-error"></i><div>Something went wrong, please try again later.</div>').fadeIn();
-            
+                    $('.contact-box-hide').slideUp();
+                    $('.contact-message').html('<i class="fa fa-check contact-success"></i><div>Your message has been sent.</div>').fadeIn();
+                } else {
+                    $('.btn-contact-container').hide();
+                    $('.contact-message').html('<i class="fa fa-exclamation contact-error"></i><div>Something went wrong, please try again later.</div>').fadeIn();
+
+                }
+            });
         }
-      });
+    });
+
+
+    function validateEmail(sEmail) {
+        var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        if (filter.test(sEmail)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-  });  
- 
-         
-  function validateEmail(sEmail) {
-    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    if (filter.test(sEmail)) {
-      return true;
-    } else {
-      return false;
-    }
-  } 
- 
+
 
 	/* Contact End
 	-------------------------------------------------------------------*/
 
 
-    
 
-    
+
+
 
 
 
@@ -238,13 +238,12 @@ jQuery(document).ready(function($) {
 -------------------------------------------------------------------*/
 
 
-/* Preloder 
+/* Preloder
 -------------------------------------------------------------------*/
-$(window).load(function () {    
+$(window).load(function () {
     "use strict";
     $("#loader").fadeOut();
     $("#preloader").delay(350).fadeOut("slow");
 });
  /* Preloder End
 -------------------------------------------------------------------*/
-   
